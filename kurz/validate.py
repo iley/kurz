1a40d80891df2f2aaf8827b3fd820e51bbe9b8d4
Ilya Strukov <iley@iley.ru>
2020-02-22 16:43:37 +0100

    Improve navigation, make validation more strict

--- kurz/validate.py
+++ kurz/validate.py
@@ -12,7 +12,7 @@ class ValidationError(Exception):
 
 
 LINK_RE = re.compile(r"[A-Za-z0-9_-]+")
-LINK_BLACKLIST = ("admin", "create", "edit", "user", "links")
+LINK_BLACKLIST = ("admin", "user", "create", "edit", "links", "delete")
 
 
 def validate_link_id(link_id):
@@ -23,6 +23,10 @@ def validate_link_id(link_id):
             "Link name %s contains invalid characters. " % link_id +
             "Valid characters are letters, numbers, - and _."
         )
+    if len(link_id) < 2:
+        raise ValidationError("Link must be at least 2 characters long")
+    if not link_id[0].isalnum():
+        raise ValidationError("Link must start with letter or digit")
     if link_id in LINK_BLACKLIST:
         raise ValidationError("Link name %s not allowed" % link_id)
 
